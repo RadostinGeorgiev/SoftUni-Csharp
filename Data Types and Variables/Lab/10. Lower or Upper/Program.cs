9e50ad40df94b1b7322379df109b606bd52eaa43
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-06-01 06:17:05 +0300

    Initial commit

---
+++ Data Types and Variables/Lab/10. Lower or Upper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char currentSymbol = char.Parse(Console.ReadLine());
+
+            if (currentSymbol >= 65 && currentSymbol <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if (currentSymbol >= 97 && currentSymbol <= 122)
+            {
+                Console.WriteLine("lower-case");
+            }
+
+        }
+    }
+}
