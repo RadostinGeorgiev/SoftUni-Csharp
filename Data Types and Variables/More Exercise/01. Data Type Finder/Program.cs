6ee3f27efd8323b1175eedef86e7da4d3c6bf11c
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-06-01 06:20:09 +0300

    Add More Exercise

---
+++ Data Types and Variables/More Exercise/01. Data Type Finder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01._Data_Type_Finder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string dataType = "";
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out _))
+                {
+                    dataType = "integer";
+                }
+                else if (double.TryParse(input, out _))
+                {
+                    dataType = "floating point";
+                }
+                else if (char.TryParse(input, out _))
+                {
+                    dataType = "character";
+                }
+                else if (bool.TryParse(input, out _))
+                {
+                    dataType = "boolean";
+                }
+                else
+                {
+                    dataType = "string";
+                }
+
+                Console.WriteLine($"{input} is {dataType } type");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Data Types and Variables/More Exercise/04. Refactoring Prime Checker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _04._Refactoring_Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= maxNumber; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j*j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isPrime.ToString().ToLower()}");
+            }
+        }
+    }
+}
