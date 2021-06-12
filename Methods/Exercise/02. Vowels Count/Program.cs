14fdb608aa3fbf619c80ee3a2cd2a39d57541348
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-06-13 00:17:48 +0300

    Add Exercise

---
+++ Methods/Exercise/02. Vowels Count/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(CountVowel(input));
+        }
+
+        static int CountVowel(string input)
+        {
+            int vowelsCounter = 0;
+            input = input.ToLower();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                switch (currentChar)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'o':
+                    case 'u':
+                    case 'i':
+                        vowelsCounter++;
+                        break;
+                }
+            }
+
+            return vowelsCounter;
+        }
+    }
+}
---
+++ Methods/Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Substract(Sum(number1, number2), number3));
+        }
+
+        static int Sum(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+
+        static int Substract(int input1, int input2)
+        {
+            return input1 - input2;
+        }
+    }
+}
