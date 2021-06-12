9790aa70a445759634f58124ffbfb39955157338
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-06-13 00:15:34 +0300

    Initial commit

---
+++ Methods/Lab/03. Calculations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "add":
+                    AddNumbers(number1, number2);
+                    break;
+                case "multiply":
+                    MultiplyNumbers(number1, number2);
+                    break;
+                case "subtract":
+                    SubtractNumbers(number1, number2);
+                    break;
+                case "divide":
+                    DivideNumbers(number1, number2);
+                    break;
+            }
+
+            static void AddNumbers(int input1, int input2)
+            {
+                Console.WriteLine(input1 + input2);
+            }
+
+            static void MultiplyNumbers(int input1, int input2)
+            {
+                Console.WriteLine(input1 * input2);
+            }
+
+            static void SubtractNumbers(int input1, int input2)
+            {
+                Console.WriteLine(input1 - input2);
+            }
+
+            static void DivideNumbers(int input1, int input2)
+            {
+                Console.WriteLine(input1 / input2);
+            }
+
+        }
+    }
+}
---
+++ Methods/Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = int.Parse(Console.ReadLine());
+            double length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RectangleArea(width, length));
+        }
+
+        static double RectangleArea(double width, double length)
+        {
+            return width * length;
+        }
+    }
+}
