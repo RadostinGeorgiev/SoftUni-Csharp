83dd6e9b53793122c272651e0e480342fcbc4457
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-06-19 21:57:39 +0300

    Initial commit

---
+++ Lists/Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            int length = numbers.Count / 2;
+            for (int i = 0; i < length; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
---
+++ Lists/Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int> secondList = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>(firstList.Count + secondList.Count);
+
+            
+            for (int i = 0; i < result.Capacity; i++)
+            {
+                if (i < firstList.Count)
+                {
+                    result.Add(firstList[i]);
+                }
+                if (i < secondList.Count)
+                {
+                    result.Add(secondList[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
