38ab00dced7e7259129d97ab2d0559ec99268b63
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-08-21 21:01:21 +0300

    Initial commit

---
+++ Exam Preparation/Final Exams/Retake-Exam-24.03.2019/4.International SoftUniada/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4.International_SoftUniada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, Dictionary<string, int>> contestants = new Dictionary<string, Dictionary<string, int>>();
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] text = input
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string countryName = text[0];
+                string contestantName = text[1];
+                int contestantPoints = int.Parse(text[2]);
+
+                if (!contestants.ContainsKey(countryName))
+                {
+                    contestants.Add(countryName, new Dictionary<string, int>() { { contestantName, contestantPoints } });
+                }
+                else
+                {
+                    if (!contestants[countryName].ContainsKey(contestantName))
+                    {
+                        contestants[countryName].Add(contestantName, contestantPoints);
+                    }
+                    else
+                    {
+                        contestants[countryName][contestantName] += contestantPoints;
+                    }
+                }
+            }
+
+            foreach (var keyValuePair in contestants
+                .OrderByDescending(x=>x.Value.Values.Sum()))
+            {
+                Console.WriteLine($"{keyValuePair.Key}: {keyValuePair.Value.Values.Sum()}");
+                foreach (var valuePair in keyValuePair.Value)
+                {
+                    Console.WriteLine($"-- {valuePair.Key} -> {valuePair.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Exam Preparation/Final Exams/Retake-Exam-24.03.2019/Emoji Sumator/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2.Emoji_Sumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[\s,.!?](?<emoji>:(?<word>[a-z]{4,}):)[\s,.!?]";
+            string message = Console.ReadLine();
+
+            int[] code = Console.ReadLine()
+                .Split(':', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            string codeWord = String.Empty;
+
+            foreach (var i in code)
+            {
+                codeWord += (char)i;
+            }
+
+            bool isFound = false;
+            int power = 0;
+
+            if (Regex.IsMatch(message, pattern))
+            {
+                var matches = Regex.Matches(message, pattern);
+
+                foreach (Match emoji in matches)
+                {
+                    string word = emoji.Groups["word"].Value;
+                    if (word == codeWord)
+                    {
+                        isFound = true;
+                    }
+
+                    for (var i = 0; i < word.Length; i++)
+                    {
+                        power += (int)word[i];
+                    }
+                }
+
+                Console.WriteLine($"Emojis found: {string.Join(", ", matches.Select(x => x.Groups["emoji"].Value))}");
+
+                if (isFound)
+                {
+                    power *= 2;
+                }
+            }
+
+            Console.WriteLine($"Total Emoji Power: {power}");
+        }
+    }
+}
