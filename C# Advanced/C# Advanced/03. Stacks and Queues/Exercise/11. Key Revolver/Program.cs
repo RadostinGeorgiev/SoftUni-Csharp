db00c0b179ce968826036e3171c86e99180eef10
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-08-27 23:01:14 +0300

    Finish solution

--- C# Advanced/C# Advanced/03. Stacks and Queues/Exercise/11. Key Revolver/Program.cs
+++ C# Advanced/C# Advanced/03. Stacks and Queues/Exercise/11. Key Revolver/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11._Key_Revolver
 {
@@ -6,7 +8,37 @@ namespace _11._Key_Revolver
     {
         static void Main(string[] args)
         {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
+            Stack<int> bullets = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Queue<int> locks = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int intelligence = int.Parse(Console.ReadLine());
+            int bulletsCosts = 0;
+            int shots = 0;
 
+            while ((bullets.Count > 0) && (locks.Count > 0))
+            {
+                bulletsCosts += bulletPrice;
+                shots++;
+                if (bullets.Pop() <= locks.Peek())
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                if ((shots % barrelSize == 0) && (bullets.Count > 0))
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+
+            Console.WriteLine(locks.Count == 0
+                ? $"{bullets.Count} bullets left. Earned ${intelligence - bulletsCosts}"
+                : $"Couldn't get through. Locks left: {locks.Count}");
         }
     }
 }
---
+++ C# Advanced/C# Advanced/03. Stacks and Queues/Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> cups = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Stack<int> bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int wastedLittersOfWater = 0;
+
+            while ((bottles.Count > 0) && (cups.Count > 0))
+            {
+                int currentBottle = bottles.Pop();
+                int currentCup = cups.Peek();
+
+                while (currentCup > 0)
+                {
+                    currentCup -= currentBottle;
+                    if (currentCup <= 0)
+                    {
+                        wastedLittersOfWater -= currentCup;
+                        cups.Dequeue();
+                    }
+                    else
+                    {
+                        currentBottle = bottles.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine(bottles.Count > 0
+                ? $"Bottles: {string.Join(' ', bottles)}"
+                : $"Cups: {string.Join(' ', cups)}");
+            Console.WriteLine($"Wasted litters of water: {wastedLittersOfWater}");
+        }
+    }
+}
