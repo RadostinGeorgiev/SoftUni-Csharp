4d34cc3d760718f1b48877317dc6d67f66daa291
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-08-30 07:40:17 +0300

    Initial commit

---
+++ C# Advanced/C# Advanced/05. Multidimensional Arrays/Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size, size];
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < size; row++)
+            {
+                int[] currentRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                
+                for (int col = 0; col < size; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+
+                    if (row == col)
+                    {
+                        primaryDiagonalSum += matrix[row, col];
+                    }
+                    if (size - 1 - row == col)
+                    {
+                        secondaryDiagonalSum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/05. Multidimensional Arrays/Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rows = matrixDimensions[0];
+            int columns = matrixDimensions[1];
+            string[,] matrix = new string[rows, columns];
+            int equalCellsCounter = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] currentRow = Console.ReadLine().Split();
+                for (int col = 0; col < columns; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+
+                    if ((row > 0) && (col > 0))
+                    {
+                        if ((matrix[row, col] == matrix[row, col - 1])
+                        && (matrix[row, col] == matrix[row - 1, col])
+                        && (matrix[row, col] == matrix[row - 1, col - 1]))
+                        {
+                            equalCellsCounter++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(equalCellsCounter);
+        }
+    }
+}
