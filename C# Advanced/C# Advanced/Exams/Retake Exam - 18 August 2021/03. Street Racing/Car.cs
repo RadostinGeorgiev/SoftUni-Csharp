f1383e91117980486e54d8235181eee1173d3a3a
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2021-10-10 22:33:05 +0300

    Commit C# Advanced Retake Exam - 18 August 2021

---
+++ C# Advanced/C# Advanced/Exams/Retake Exam - 18 August 2021/03. Street Racing/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            HorsePower = horsePower;
+            LicensePlate = licensePlate;
+            Make = make;
+            Model = model;
+            Weight = weight;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb
+                .AppendLine($"Make: {Make}")
+                .AppendLine($"Model: {Model}")
+                .AppendLine($"License Plate: {LicensePlate}")
+                .AppendLine($"Horse Power: {HorsePower}")
+                .AppendLine($"Weight: {Weight}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced/Exams/Retake Exam - 18 August 2021/03. Street Racing/Race.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    class Race
+    {
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Capacity = capacity;
+            Laps = laps;
+            MaxHorsePower = maxHorsePower;
+            Name = name;
+            Type = type;
+        }
+
+        public Dictionary<string, Car> Participants { get; set; } = new Dictionary<string, Car>();
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+
+        public int Count => Participants.Count;
+
+        public void Add(Car car)
+        {
+            if (Participants.Count < Capacity &&
+                !Participants.ContainsKey(car.LicensePlate) &&
+                car.HorsePower <= MaxHorsePower)
+            {
+                Participants.Add(car.LicensePlate, car);
+            }
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            return Participants.ContainsKey(licensePlate) && Participants.Remove(licensePlate);
+        }
+
+        public Car FindParticipant(string licensePlate)
+        {
+            return Participants.ContainsKey(licensePlate) ? Participants[licensePlate] : null;
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            return Participants.Count > 0 ? 
+                Participants.First(x => x.Value.HorsePower == 
+                                        Participants.Max(x => x.Value.HorsePower)).Value 
+                : null;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+
+            foreach (var kVP in Participants)
+            {
+                sb.AppendLine(kVP.Value.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
