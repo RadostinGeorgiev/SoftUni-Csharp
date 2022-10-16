7be4a2e9f836d61fa5aabcfe9b5612404bf7c1dd
Radostin Georgiev <radostin.r.georgiev@gmail.com>
2022-10-16 23:05:13 +0300

    Initial commit

---
+++ C# Algorithms/Advanced/06. Graphs Strongly Connected Components, Max Flow - Exercise/01. Electrical Substation Network/Program.cs
@@ -0,0 +1,139 @@
+﻿namespace _01._Electrical_Substation_Network
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Graph
+    {
+        public int VerticesCount { get; set; }
+        public List<int>[] Nodes;
+
+        public Graph(int verticesCount)
+        {
+            this.VerticesCount = verticesCount;
+            this.Nodes = new List<int>[VerticesCount];
+
+            for (int node = 0; node < this.Nodes.Length; node++)
+            {
+                this.Nodes[node] = new List<int>();
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static bool[] visited;
+
+        static void Main(string[] args)
+        {
+            int nodes = int.Parse(Console.ReadLine());
+            int lines = int.Parse(Console.ReadLine());
+
+            Graph graph = new Graph(nodes);
+            graph = ReadGraphNodesFromConsole(graph, lines);
+
+            visited = new bool[nodes];
+            List<Stack<int>> components = FindStronglyConnectedComponents(graph);
+
+            components.ForEach(c => Console.WriteLine($"{string.Join(", ", c)}"));
+        }
+
+        private static Graph ReadGraphNodesFromConsole(Graph graph, int lines)
+        {
+            for (int i = 0; i < lines; i++)
+            {
+                int[] line = Console.ReadLine()
+                                    .Split(", ")
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+                int node = line[0];
+                graph.Nodes[node].AddRange(line.Skip(1));
+            }
+
+            return graph;
+        }
+
+        private static Graph BuildReverseGraph(Graph graph)
+        {
+            Graph reversedGraph = new Graph(graph.VerticesCount);
+
+            for (int node = 0; node < graph.VerticesCount; node++)
+            {
+                reversedGraph.Nodes[node] = new List<int>();
+            }
+
+            for (int node = 0; node < graph.VerticesCount; node++)
+            {
+                foreach (int child in graph.Nodes[node])
+                {
+                    reversedGraph.Nodes[child].Add(node);
+                }
+            }
+
+            return reversedGraph;
+        }
+
+        private static Stack<int> TopologicalSorting(Graph graph)
+        {
+            Stack<int> sorted = new Stack<int>();
+
+            for (int node = 0; node < graph.VerticesCount; node++)                  //DFS for all nodes in graph
+            {
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                Dfs(node, graph, sorted);
+            }
+
+            return sorted;
+        }
+
+        private static void Dfs(int node, Graph graph, Stack<int> result)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (int child in graph.Nodes[node])
+            {
+                Dfs(child, graph, result);
+            }
+
+            result.Push(node);
+        }
+
+        private static List<Stack<int>> FindStronglyConnectedComponents(Graph originalGraph)
+        {
+            var stronglyConnectedComponents = new List<Stack<int>>();
+
+            Stack<int> sorted = TopologicalSorting(originalGraph);
+            Graph reversedGraph = BuildReverseGraph(originalGraph);
+
+            visited = new bool[originalGraph.VerticesCount];
+
+            while (sorted.Count != 0)                                               //DFS for all nodes in topological sorted elements
+            {
+                int node = sorted.Pop();
+
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                Stack<int> elements = new Stack<int>();
+                Dfs(node, reversedGraph, elements);
+
+                stronglyConnectedComponents.Add(elements);
+            }
+
+            return stronglyConnectedComponents;
+        }
+    }
+}
---
+++ C# Algorithms/Advanced/06. Graphs Strongly Connected Components, Max Flow - Exercise/02. Maximum Tasks Assignment/Program.cs
@@ -0,0 +1,192 @@
+﻿namespace _02._Maximum_Tasks_Assignment
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Graph
+    {
+        public int VerticesCount { get; set; }
+        public int[,] Nodes;
+
+        public Graph(int verticesCount)
+        {
+            this.VerticesCount = verticesCount;
+            this.Nodes = new int[verticesCount, verticesCount];
+        }
+    }
+
+    internal class Program
+    {
+        private static int people;
+        private static int nodes;
+        private static Graph graph;
+        private static int[] parents;
+
+        static void Main(string[] args)
+        {
+            people = int.Parse(Console.ReadLine());
+            int tasks = int.Parse(Console.ReadLine());
+
+            graph = CreateGraphFromConsole(people, tasks);
+
+            parents = new int[nodes];
+            Array.Fill(parents, -1);
+
+            int source = 0;
+            int destination = nodes - 1;
+
+            ReconstructPath(source, destination);
+
+            GetMaximumAssignment(people, tasks);
+
+            //GetMaximumAssignmentBfs(source, destination);
+            //Console.WriteLine($"Max flow = {maxFlow}");
+        }
+
+        private static void GetMaximumAssignment(int people, int tasks)
+        {
+            SortedSet<string> result = new SortedSet<string>();
+
+            for (int task = people + 1; task <= people + tasks; task++)
+            {
+                for (int index = 0; index < graph.Nodes.GetLength(1); index++)
+                {
+                    if (graph.Nodes[task, index] > 0)
+                    {
+                        result.Add($"{(char)(index - 1 + 'A')}-{task - people}");
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, result));
+        }
+
+        private static void GetMaximumAssignmentBfs(int source, int destination)
+        {
+            SortedSet<string> result = new SortedSet<string>();
+            bool[] visited = new bool[nodes];
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(destination);
+            visited[destination] = true;
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+
+                for (int child = 0; child < graph.Nodes.GetLength(1); child++)
+                {
+                    if (!visited[child] && graph.Nodes[node, child] == 1)
+                    {
+                        queue.Enqueue(child);
+                        visited[child] = true;
+
+                        if (node != source && node != destination &&
+                            child != source && child != destination)
+                        {
+                            result.Add($"{(char)(child - 1 + 'A')}-{node - people}");
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static Graph CreateGraphFromConsole(int people, int tasks)
+        {
+            nodes = people + tasks + 2;
+            Graph graph = new Graph(nodes);
+
+            //Graph => Source, People1, People2, .. PeopleN, Task1, Task2, ... TaskN, Destination
+            //Graph => Source, Peoples, Tasks, Destination
+            //Graph.nodes => Peoples + Tasks + 2
+
+            for (int person = 1; person <= people; person++)
+            {
+                graph.Nodes[0, person] = 1;
+            }
+
+            for (int task = people + 1; task <= people + tasks; task++)
+            {
+                graph.Nodes[task, nodes - 1] = 1;
+            }
+
+            for (int person = 1; person <= people; person++)
+            {
+                string peronTasks = Console.ReadLine();
+
+                for (int task = 0; task < peronTasks.Length; task++)
+                {
+                    graph.Nodes[person, people + task + 1] = peronTasks[task] == 'Y' ? 1 : 0;
+                }
+            }
+
+            return graph;
+        }
+
+        private static bool Bfs(int source, int destination)
+        {
+            bool[] visited = new bool[nodes];
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(source);
+            visited[source] = true;
+
+            while (queue.Count > 0)
+            {
+                int node = queue.Dequeue();
+
+                for (int child = 0; child < graph.Nodes.GetLength(1); child++)
+                {
+                    if (!visited[child] && graph.Nodes[node, child] == 1)
+                    {
+                        queue.Enqueue(child);
+                        visited[child] = true;
+                        parents[child] = node;
+                    }
+                }
+            }
+
+            return visited[destination];
+        }
+
+        private static void UpdateFlow(int source, int destination, int flow)
+        {
+            int node = destination;
+
+            while (node != source)
+            {
+                int parent = parents[node];
+                graph.Nodes[parent, node] -= flow;
+                graph.Nodes[node, parent] += flow;
+                node = parent;
+            }
+        }
+
+        private static void ReconstructPath(int source, int destination)
+        {
+            int maxFlow = 0;
+
+            while (Bfs(source, destination))
+            {
+                UpdateFlow(source, destination, 1);
+                maxFlow += 1;
+            }
+        }
+
+        public static void PrintGraph(Graph graph)                                  //only for debugging
+        {
+            for (int row = 0; row < graph.Nodes.GetLength(0); row++)
+            {
+                for (int col = 0; col < graph.Nodes.GetLength(1); col++)
+                {
+                    var result = graph.Nodes[row, col] == 1 ? 'Y' : 'N';
+                    Console.Write($"{result} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
